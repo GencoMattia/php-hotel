1274858639cfedf282e9019b7a1f0a280f3ff29d
GencoMattia <gencomattia@hotmail.it>
2024-06-27 15:55:08 +0200

    added basic structure with card title

--- index.php
+++ index.php
@@ -3,3 +3,25 @@
 require_once __DIR__ . "/utilities/hotel-list.php";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-hotel</title>
+</head>
+<body>
+    <main>
+        <section class="hotel-container">
+            <?php foreach($hotels as $hotel) { ?>
+                <article class="hotel-card">
+                    <h2>
+                        <?php echo $hotel["name"] ?>
+                    </h2>
+                </article>
+            <?php } ?>
+        </section>
+    </main>
+</body>
+</html>
